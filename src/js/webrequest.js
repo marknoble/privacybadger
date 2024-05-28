e2c1e5992bf42dcbccb8a084517aa4478d873ed3
Alexei <alexeiatyahoodotcom@gmail.com>
2024-05-28 17:17:20 -0400

    Use chrome.scripting APIs to send GPC/DNT in JS

--- src/js/webrequest.js
+++ src/js/webrequest.js
@@ -1190,7 +1190,6 @@ function dispatcher(request, sender, sendResponse) {
     // reject unless it's a known content script message
     const KNOWN_CONTENT_SCRIPT_MESSAGES = [
       "allowWidgetOnSite",
-      "checkDNT",
       "checkEnabled",
       "checkLocation",
       "checkWidgetReplacementEnabled",
@@ -1823,16 +1822,6 @@ function dispatcher(request, sender, sendResponse) {
   case "removeErrorText": {
     let activeTab = badger.tabData._tabData[request.tabId];
     delete activeTab.errorText;
-    break;
-  }
-
-  // called from contentscripts/dnt.js
-  // to check if it should set DNT on Navigator
-  case "checkDNT": {
-    let tab_host = extractHostFromURL(sender.tab.url);
-    sendResponse(
-      badger.isDntSignalEnabled(tab_host) &&
-      badger.isPrivacyBadgerEnabled(tab_host));
     break;
   }
 
